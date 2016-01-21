d8038979b6f73ea2aaa8230e8532257516282ae5
agervail <antoine.gervail@probayes.com>
2016-01-21 11:14:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, url_for, request, redirect
+import subprocess
+import time
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    return "Hello You"
+
+@app.route("/move_servo")
+def move_servo():
+  if 'x' in request.args and 'y' in request.args:
+    try:
+      x = 180 - int(request.args['x'])
+      y = 180 - int(request.args['y'])
+      subprocess.Popen(["python", "write_pan_tilt.py", "-p", str(x), "-t", str(y)])
+    except:
+      pass
+  return redirect(url_for('cam'))
+
+@app.route("/cam")
+def cam():
+  url_for('static', filename='current.jpg')
+  url_for('static', filename='target.png')
+  url_for('static', filename='pos.json')
+  return render_template('cam.html', img_src='static/current.jpg?' + str(int(time.time())))
+
+if __name__ == "__main__":
+    #app.debug = True
+    #app.run(host='0.0.0.0')
+    app.run()
---
+++ write_pan_tilt.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+import sys, getopt
+import smbus
+import time
+import fcntl
+import json
+
+bus = smbus.SMBus(1)
+
+# This is the address we setup in the Arduino Program
+address = 0x04
+
+def writeNumber(value):
+  bus.write_byte(address, value)
+  # bus.write_byte_data(address, 0, value)
+  return -1
+
+def main(argv):
+  pan = ''
+  tilt = ''
+  try:
+    opts, args = getopt.getopt(argv,"hp:t:",["pan=","tilt="])
+  except getopt.GetoptError:
+    print 'write_pan_tilt.py --pan <pan_value> --tilt <tilt_value>'
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt == '-h':
+      print 'write_pan_tilt.py --pan <pan_value> --tilt <tilt_value>'
+      sys.exit()
+    elif opt in ("-p", "--pan"):
+      pan = arg
+    elif opt in ("-t", "--tilt"):
+      tilt = arg
+
+  lower_bound = 30
+  upper_bound = 150
+  old_x = 90
+  old_y = 90
+  handle_pos = open('lock_pos_file', 'a')
+  fcntl.flock(handle_pos.fileno(), fcntl.LOCK_EX)
+  with open('static/pos.json', 'r') as f:
+    data = json.load(f)
+    old_x = data.get('x', 90)
+    old_y = data.get('y', 90)
+  handle_pos.close()
+  print old_x, old_y
+
+  if pan and int(pan) >= lower_bound and int(pan) <= upper_bound:
+    old_x = 180 - int(pan)
+    handle = open('lock_servo', 'a')
+    fcntl.flock(handle.fileno(), fcntl.LOCK_EX)
+    time.sleep(0.1)
+    writeNumber(200)
+    time.sleep(0.1)
+    writeNumber(int(pan))
+    handle.close()
+    print 'pan "', pan
+  if tilt and int(tilt) >= lower_bound and int(tilt) <= upper_bound:
+    old_y = 180 - int(tilt)
+    handle = open('lock_servo', 'a')
+    fcntl.flock(handle.fileno(), fcntl.LOCK_EX)
+    time.sleep(0.1)
+    writeNumber(210)
+    time.sleep(0.1)
+    writeNumber(int(tilt))
+    handle.close()
+    print 'tilt "', tilt
+
+  handle_pos = open('lock_pos_file', 'a')
+  fcntl.flock(handle_pos.fileno(), fcntl.LOCK_EX)
+  with open('static/pos.json', 'w') as f:
+    data = {'x': old_x, 'y': old_y}
+    json.dump(data, f)
+  handle_pos.close()
+
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
